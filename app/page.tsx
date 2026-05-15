4f0540bdbe33fed1148612f9ae1b7be21cca1415
proiect2026-art <proiect2026@gmail.com>
2026-05-15 20:57:04 +0300

    Add main homepage

---
+++ app/page.tsx
@@ -0,0 +1,8 @@
+export default function HomePage() {
+  return (
+    <main style={{padding: 40, fontFamily: 'Arial'}}>
+      <h1>Number Line App</h1>
+      <p>Aplicație educațională interactivă pentru axă numerică.</p>
+    </main>
+  )
+}
